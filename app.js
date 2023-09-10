b307b58e916315c4711d2f5c9d2a54560a6a1840
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-10 15:52:33 +0100

    Add post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -13,6 +14,20 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  console.log(newTour);
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Port is running on port ${PORT}`);
