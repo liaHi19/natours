766844ed5961f60d8bfb19a933876c6ed09b3003
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-10 18:05:10 +0100

    Add get request with params

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Incorrect ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
