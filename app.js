7d6a33812887a0e73b0683839a4556c3f654af9d
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-10 18:36:23 +0100

    fix create separate functions

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const express = require('express');
-const { log } = require('console');
 
 const app = express();
 app.use(express.json());
@@ -9,24 +8,13 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getTours = (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const id = +req.params.id;
-  const tour = tours.find((tour) => tour.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Incorrect ID' });
-  }
-
-  res.status(200).json({ status: 'success', data: { tour } });
-});
-
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
   tours.push(newTour);
@@ -38,9 +26,20 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
-});
+};
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Incorrect ID' });
+  }
+
+  res.status(200).json({ status: 'success', data: { tour } });
+};
+
+const deleteTour = (req, res) => {
   const id = +req.params.id;
   const tour = tours.find((tour) => tour.id === id);
 
@@ -55,7 +54,10 @@ app.delete('/api/v1/tours/:id', (req, res) => {
       res.status(204).json({ status: 'success', data: null });
     }
   );
-});
+};
+
+app.route('/api/v1/tours').get(getTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).delete(deleteTour);
 
 const PORT = 3000;
 app.listen(PORT, () => {
