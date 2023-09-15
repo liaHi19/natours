810600f4efad60b532f0369b2adff1d1476100e5
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-09-15 09:44:08 +0100

    Add env-variables

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
