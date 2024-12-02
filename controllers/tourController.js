d099b8f27e2b2175335ad527add99fa4234abe90
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-12-02 16:41:57 +0000

    feat: adding update, read and delete operation to the tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,17 @@
 const Tour = require('./../models/tourModel');
 
-exports.getTours = (req, res) => {
-  const { requestTime } = req;
-  // res.status(200).json({
-  //   status: 'success',
-  //   results: tours.length,
-  //   requestTime,
-  //   data: { tours },
-  // });
+exports.getTours = async (req, res) => {
+  const tours = await Tour.find();
+
+  try {
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: error });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -19,19 +23,36 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.getTour = (req, res) => {
-  const id = +req.params.id;
-  // const tour = tours.find((tour) => tour.id === id);
-
-  // res.status(200).json({ status: 'success', data: { tour } });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    res.status(200).json({ status: 'success', data: { tour } });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: 'Invalid data sent' });
+  }
 };
 
-exports.updateTour = (req, res) => {
-  res
-    .status(500)
-    .json({ status: 'error', message: 'This route is not defined' });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ status: 'success', data: { tour } });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: error });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  const id = +req.params.id;
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      message: `A tour with id: ${req.params.id} has been deleted successfully`,
+    });
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: error });
+  }
 };
