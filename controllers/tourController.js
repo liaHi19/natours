d1070acdcd235acee240f9e69e9e378a9eb11ab7
Nataliia Hirniak <gn.natalia19@gmail.com>
2024-12-05 11:23:14 +0000

    refactor: adding utility class APIFeatures

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTours = async (req, res, next) => {
   req.query.limit = '5';
@@ -9,50 +10,13 @@ exports.aliasTopTours = async (req, res, next) => {
 };
 
 exports.getTours = async (req, res) => {
-  const queryObj = { ...req.query };
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-  // basic query
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-
-  // correct inputs
-  let queryStr = JSON.stringify(queryObj);
-  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-  let query = Tour.find(JSON.parse(queryStr));
-
-  // {difficulty: "easy", duration: {$gte: 5}}
-
-  // sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort('-createdAt');
-  }
-
-  // field limiting
-  if (req.query.fields) {
-    const fields = req.query.fields.split(',').join(' ');
-    query = query.select(fields);
-  } else {
-    query = query.select('-__v');
-  }
-
-  // pagination
-  const page = req.query.page * 1 || 1;
-  const limit = req.query.limit * 1 || 100;
-
-  const skip = (page - 1) * limit;
-
-  if (req.query.page) {
-    const numTours = await Tour.countDocuments();
-    if (skip >= numTours) throw new Error('This page does not exist');
-  }
-
-  query = query.skip(skip).limit(limit);
-
-  const tours = await query;
+  const tours = await features.query;
 
   try {
     res.status(200).json({
